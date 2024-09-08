c854f2dcdf564b55b6b38806244d3d674014cee8
uwe77 <uwe90711@gmail.com>
2024-09-09 00:36:24 +0800

    feat gymnasium_arg install constrain

--- gymnasium/setup.py
+++ gymnasium/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gymnasium_arg',
       version='0.0.1',
-      install_requires=['gymnasium']  # And any other dependencies foo needs
+      install_requires=['gymnasium', 'aerialgym_arg']  # And any other dependencies foo needs
 )
